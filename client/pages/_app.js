224a998f34c56ac52d31339570a9043e0c9a0486
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-12-05 11:27:15 +0100

    add wagmi and start to implement profile component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,44 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import {
+  WagmiConfig,
+  createClient,
+  defaultChains,
+  configureChains,
+} from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
+  alchemyProvider({ apiKey: process.env.ALCHEMY_GOERLI_KEY }),
+  publicProvider(),
+]);
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <WagmiConfig client={client}>
       <Head>
         <title>CLEARN</title>
         <meta name="description" content="Epargne solidaire" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </WagmiConfig>
   );
 }
 
