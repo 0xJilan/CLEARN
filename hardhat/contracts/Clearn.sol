faf295b28bd111d79cf9834a0a543f74b9e144a0
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-12-06 15:54:51 +0100

    add doc to CLEARN contract

--- hardhat/contracts/Clearn.sol
+++ hardhat/contracts/Clearn.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-
 import "hardhat/console.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,38 +7,49 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @title Clearn Token
 /// @author @0xJilan
 /// @notice ERC20 implementation which serves as proof of deposit in the treasury
-
 contract Clearn is ERC20, Ownable {
-    error NotOwner();
-    error MinterSet();
-    //TODO: Add Function documentation
-    //TODO: Add Events for setMinter,  debitFrom, creditTo
-
     /// @notice Only treasury address can mint tokens
     address public minter;
 
+    event SetMinter(address _minter);
+    event Mint(address _toAddress, uint256 _amount);
+    event Burn(address _from, uint256 _amount);
+
     constructor() ERC20("Clearn", "CLEARN") {}
 
+    /// @notice Check if sender is Minter;
     modifier onlyMinter() {
         require(msg.sender == minter, "Not minter");
         _;
     }
 
+    /// @notice Set minter address if called by Owner;
+    /// @param _minter treasury address that will controls mint and burn;
     function setMinter(address _minter) external onlyOwner {
         minter = _minter;
+        emit SetMinter(_minter);
     }
 
-    function debitFrom(
-        address _from,
-        uint256 _amount
-    ) external virtual onlyMinter {
-        _burn(_from, _amount);
-    }
+    /// @notice Mint CLEARN for an address if called by Minter;
+    /// @param _toAddress address to be credited by mint;
+    /// @param _amount amount of CLEARN to mint;
 
     function creditTo(
         address _toAddress,
         uint256 _amount
     ) external virtual onlyMinter {
         _mint(_toAddress, _amount);
+        emit Mint(_toAddress, _amount);
+    }
+
+    /// @notice Burn CLEARN of a particular address if called by Minter;
+    /// @param _from address to be debited by burn;
+    /// @param _amount amount of CLEARN to burn;
+    function debitFrom(
+        address _from,
+        uint256 _amount
+    ) external virtual onlyMinter {
+        _burn(_from, _amount);
+        emit Burn(_from, _amount);
     }
 }
